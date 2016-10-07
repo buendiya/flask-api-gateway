63d20bf2582892ec87fcc52f7495ce47e719bc57
buendiya <jingshuaizhen@gmail.com>
2016-10-07 17:20:22 +0800

    update

--- api_gateway/CONFIGS.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Redis 配置
-REDIS_HOST = '121.41.4.137'
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_PASSWORD = '7180ce47d71ec0269ccbfa734093350ea444e2b9f20539fad6571cf7788211f4'
